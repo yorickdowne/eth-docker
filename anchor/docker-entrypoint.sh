8b20535894d32141f2239c0b25471751c35c6d27
yorickdowne <71337066+yorickdowne@users.noreply.github.com>
2025-07-25 11:59:04 +0200

    Add QUIC port to Anchor (#2248)

--- anchor/docker-entrypoint.sh
+++ anchor/docker-entrypoint.sh
@@ -8,7 +8,7 @@ fi
 
 if [ "${IPV6}" = "true" ]; then
   echo "Configuring Anchor to listen on IPv6 ports"
-  __ipv6="--listen-addresses :: --port6 ${SSV_P2P_PORT:-13001} --discovery-port6 ${SSV_P2P_PORT_UDP:-12001}"
+  __ipv6="--listen-addresses :: --port6 ${SSV_P2P_PORT:-13001} --discovery-port6 ${SSV_P2P_PORT_UDP:-12001} --quic-port6 ${SSV_QUIC_PORT:-13002}"
 else
   __ipv6=""
 fi
