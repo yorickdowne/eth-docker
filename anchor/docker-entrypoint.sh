a53f7e61fe1cc4bb76ec98d04fd42d5feb64c1b1
yorickdowne <71337066+yorickdowne@users.noreply.github.com>
2025-07-25 07:02:15 +0200

    Support Anchor on Hoodi (#2247)

---
+++ anchor/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+if [ "$(id -u)" = '0' ]; then
+  chown -R anchor:anchor /var/lib/anchor
+  exec gosu anchor docker-entrypoint.sh "$@"
+fi
+
+if [ "${IPV6}" = "true" ]; then
+  echo "Configuring Anchor to listen on IPv6 ports"
+  __ipv6="--listen-addresses :: --port6 ${SSV_P2P_PORT:-13001} --discovery-port6 ${SSV_P2P_PORT_UDP:-12001}"
+else
+  __ipv6=""
+fi
+
+# Word splitting is desired for the command line parameters
+# shellcheck disable=SC2086
+exec "$@" ${__ipv6} ${DVT_EXTRAS}
