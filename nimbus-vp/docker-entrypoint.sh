ea2a6a1ee005e198c2fb8ed5bbb327f457bac0a3
Yorick Downe <71337066+yorickdowne@users.noreply.github.com>
2026-07-01 12:18:38 +0100

    Nimbus VP can parse comma-separated URLs (#2657)

--- nimbus-vp/docker-entrypoint.sh
+++ nimbus-vp/docker-entrypoint.sh
@@ -6,25 +6,44 @@ if [[ "$(id -u)" -eq 0 ]]; then
   exec gosu user docker-entrypoint.sh "$@"
 fi
 
-
-while true; do
-  if curl -s -m 5 "${CL_NODE}" &> /dev/null; then
-    echo "Consensus Layer node is up, fetching trusted block root"
-    break
-  else
-    echo "Waiting for Consensus Layer node to be reachable..."
-    sleep 5
-  fi
+# accommodate comma separated list of consensus nodes
+nodes=$(echo "${CL_NODE}" | tr ',' ' ')
+__beacon_urls=()
+for node in ${nodes}; do
+  __beacon_urls+=("--beacon-api-url=${node}")
 done
 
-set +e
-  root=$(curl -s -f -m 30 "${CL_NODE}/eth/v1/beacon/headers/finalized" | jq -r '.data.root')
-  exitstatus=$?
-set -e
+# accommodate comma separated list of RPC URLs
+urls=$(echo "${RPC_URLS}" | tr ',' ' ')
+__rpc_urls=()
+for url in ${urls}; do
+  __rpc_urls+=("--execution-api-url=${url}")
+done
 
-if [[ "${exitstatus}" -ne 0 ]]; then
-  echo "Failed to fetch trusted block root from ${CL_NODE}"
+while true; do
+  for node in ${nodes}; do
+    if curl -s -m 5 -o /dev/null -w "%{http_code}" "${node}" | grep -q "^[23]"; then
+      echo "Consensus Layer node is up, fetching trusted block root"
+      break 2
+    fi
+  done
+  echo "Waiting for Consensus Layer node to be reachable..."
+  sleep 5
+done
+
+if ! response=$(curl -s -f -m 30 "${node}/eth/v1/beacon/headers/finalized"); then
+  echo "Failed to fetch trusted block root from ${node}"
   echo "Please verify it's reachable"
+  sleep 30
+  exit 1
+fi
+
+root=$(echo "${response}" | jq -r '.data.root')
+
+# Guard against empty or "null" results from jq
+if [[ -z "${root}" || "${root}" == "null" ]]; then
+  echo "Error: Received invalid data structure from ${node}"
+  echo "Received ${response}, which does not contain \".data.root\""
   sleep 30
   exit 1
 fi
@@ -33,7 +52,7 @@ __trusted_root="--trusted-block-root=${root}"
 i=0
 # Verified proxy can get "stuck" if light client bootstrap isn't ready. Check for it here
 while true; do
-  if curl -s -f -m 30 "${CL_NODE}/eth/v1/beacon/light_client/bootstrap/${root}" &> /dev/null; then
+  if curl -s -f -m 30 "${node}/eth/v1/beacon/light_client/bootstrap/${root}" &> /dev/null; then
     echo "Consensus Layer node has light client bootstrap available, starting Nimbus Verified Proxy"
     break
   else
@@ -56,4 +75,4 @@ done
 
 # Word splitting is desired for the command line parameters
 # shellcheck disable=SC2086
-exec "$@" ${__trusted_root} ${PROXY_EXTRAS}
+exec "$@" "${__beacon_urls[@]}" "${__rpc_urls[@]}" ${__trusted_root} ${PROXY_EXTRAS}
